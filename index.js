38464db7a5086c998036f44a45f1eb6a8b00846f
HSsuri <97456916+HSsuri@users.noreply.github.com>
2022-07-16 14:23:42 +0530

    Manage-Landing-Page

---
+++ index.js
@@ -0,0 +1,58 @@
+const formItem = document.querySelectorAll(".form._input");
+const formError = document.querySelectorAll(".form.-errorMessage");
+
+$('.Nav').click(function(){
+    $('.Nav').toggleClass("open");
+    $('.navbar-nav').toggleClass("bg-white");
+});
+function formCheck(event) {
+    
+    for (let i = 0; i < formItem.length; i++) {
+        
+        console.log(formItem[i].checkValidity())
+        if (!formItem[i].checkValidity()) {
+            
+            if (formItem[i].type == 'email') {
+                formError.innerHTML = 'Looks like this is not an email';
+            }
+            
+
+            if (!formError[i].classList.contains('-active')) {
+                formError[i].classList.toggle('-active');
+                formItem[i].classList.toggle('-error');
+            }
+
+            
+        } else {
+            formItem[i].value = '';
+            formError[i].classList.toggle('-active');
+            formItem[i].classList.toggle('-error');
+        }
+
+    }
+}
+$('form button').click(formCheck)
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    center: true,
+    margin:10,
+    nav:false,    
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            dots:true
+        },
+        575:{
+            items:2,
+            dots:true
+        },
+        860:{
+            items:3,
+            dots:false
+        }
+    }
+});
+
